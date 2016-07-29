347e5c4fd8d8e265bbade4943734875dd4066464
Sergey Timofeev <sergey.timofeev.2000@list.ru>
2016-07-29 16:10:53 +0300

    implementation of copy and paste task according to Jira BOOK-50

--- src/main/java/com/sergtm/application/copyAndPaste/SavingsBankAccount.java
+++ src/main/java/com/sergtm/application/copyAndPaste/SavingsBankAccount.java
@@ -1,5 +1,34 @@
 package com.sergtm.application.copyAndPaste;
 
 public class SavingsBankAccount {
+    private String number;
+    private double interestRate;
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
 
 }
