1eec572c2fca0fb18120e272aed19c159f4aef30
Sergey Timofeev <sergey.timofeev.2000@list.ru>
2016-08-11 12:27:43 +0300

    Implementation of tests and empty class for WhoIsBetterTask

---
+++ src/main/java/com/sergtm/application/whoIsBetter/WhoIsBetter.java
@@ -0,0 +1,10 @@
+package com.sergtm.application.whoIsBetter;
+
+/**
+ * Created by admin on 11.08.2016.
+ */
+public class WhoIsBetter {
+    public int compare(int firstNumber, int secondNumber){
+        return 0;
+    }
+}
